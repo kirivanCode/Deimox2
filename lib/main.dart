43880d4d82dec5b57484f85239d30c6aa6b94c57
BrayanJavierB <brayanj26becerra703@gmail.com>
2024-03-17 16:28:06 -0500

    decrementar

--- lib/main.dart
+++ lib/main.dart
@@ -68,6 +68,17 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _decrementar() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -116,10 +127,10 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          onPressed: _decrementar,
+          tooltip: 'decrementar',
+          child: const Icon(Icons
+              .remove)), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
