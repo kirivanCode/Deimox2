4382e45f90df313709a43d9c2267448d525df80a
BrayanJavierB <brayanj26becerra703@gmail.com>
2024-03-18 23:07:55 -0500

    investigacion

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,18 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:deimox_apli/main.dart';
+import 'package:deimox_apli/screens/pause_screen.dart'; // Importa PauseScreen
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('PauseScreen UI Test', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: PauseScreen()));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verificar si el título se muestra correctamente
+    expect(find.text('Ejercicios de Pausas Activas'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verificar si los elementos de la lista se muestran correctamente
+    expect(find.text('Estiramiento de brazos'), findsOneWidget);
+    expect(find.text('Estiramiento de piernas'), findsOneWidget);
+    expect(find.text('Respiración profunda'), findsOneWidget);
+    expect(find.text('Girar el cuello'), findsOneWidget);
   });
 }
